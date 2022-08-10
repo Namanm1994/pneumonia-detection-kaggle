3742f7759abd7a0230af8fd9d2a8cbca9f458264
Namanm1994 <namanmehta1994@gmail.com>
2022-08-10 14:24:53 +0900

    added configs and common initialize functions; transformation, gradientClip not done

--- base/pneumonia-detection-git.py
+++ base/pneumonia-detection-git.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-## Inspiration : 
+## Inspiration : https://www.kaggle.com/code/teyang/pneumonia-detection-resnets-pytorch
 
 
 # In[2]:
@@ -61,6 +61,33 @@ from torchvision import transforms, datasets
 # In[6]:
 
 
+# weights
+# epochs
+# lr
+# optimizer
+# modelFn
+# gradient_clip
+# transformation
+# layer freezing
+modelDict = {1:"Resnet18",
+             2:"Resnet50",
+             3:"Vgg19" ## Vgg always runs out of memory
+            }
+
+config={"epochs":20,
+        "lr":0.001,
+        "optimizer":torch.optim.SGD,
+        "modelFn":modelDict[2],
+        "--transformations":"",
+        "weights":True,
+        "--gradientClip":False,
+        "layerFreeze":False
+       }
+
+
+# In[7]:
+
+
 import seaborn as sns
 data_dir = '../input/chest-xray-pneumonia/chest_xray/chest_xray'
 train_samplesize = pd.DataFrame.from_dict(
@@ -74,25 +101,25 @@ sns.barplot(data=train_samplesize).set_title('Training Set Data Inbalance', font
 plt.show()
 
 
-# In[7]:
+# In[8]:
 
 
 train_samplesize
 
 
-# In[8]:
+# In[9]:
 
 
 ## Path To Data
 
 
-# In[9]:
+# In[10]:
 
 
 ll ../input/chest-xray-pneumonia/chest_xray
 
 
-# In[10]:
+# In[11]:
 
 
 train_data = '../input/chest-xray-pneumonia/chest_xray/chest_xray/train'
@@ -100,7 +127,7 @@ test_data = '../input/chest-xray-pneumonia/chest_xray/chest_xray/test'
 val_data = '../input/chest-xray-pneumonia/chest_xray/chest_xray/val'
 
 
-# In[11]:
+# In[12]:
 
 
 img_path1 = "../input/chest-xray-pneumonia/chest_xray/train/NORMAL/IM-0115-0001.jpeg"
@@ -109,13 +136,13 @@ plt.imshow(img1)
 print("shape: ", img1.shape)
 
 
-# In[12]:
+# In[13]:
 
 
 ## Find Mean and Std of Data  ../input/chest-xray-pneumonia/
 
 
-# In[13]:
+# In[14]:
 
 
 # mean = 0.
@@ -154,8 +181,16 @@ def normalise(loader):
     _std = std
     return _mean, _std
 
+# transforms = transforms.Compose([
+#     transforms.Resize(size=(256, 256)),
+#     transforms.ToTensor()
+# ])
+# dataset = datasets.ImageFolder(root = train_data, transform=transforms)
+# trainLoader = DataLoader(dataset=dataset, batch_size=trainBatchSize, shuffle=True)
+# normalise(trainLoader)
 
-# In[14]:
+
+# In[15]:
 
 
 ## Image Transforms : Normalisation does not give good results on this dataset.
@@ -169,8 +204,8 @@ train_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(p=0.4),
     transforms.RandomVerticalFlip(p=0.4),
     transforms.ToTensor(),
-    transforms.Normalize(mean = _mean,
-                         std = _std)
+#     transforms.Normalize(mean = _mean,
+#                          std = _std)
 ])
 
 val_transform = transforms.Compose([
@@ -184,13 +219,13 @@ test_transform = transforms.Compose([
 ])
 
 
-# In[15]:
+# In[16]:
 
 
 ## Create Dataset
 
 
-# In[16]:
+# In[17]:
 
 
 trainset = datasets.ImageFolder(root = train_data, transform = train_transform)
@@ -201,13 +236,13 @@ testset = datasets.ImageFolder(root = test_data, transform = test_transform)
 print(len(testset))
 
 
-# In[17]:
+# In[18]:
 
 
 ## Create Data Loader
 
 
-# In[18]:
+# In[19]:
 
 
 # trainBatchSize = 32
@@ -217,7 +252,7 @@ val_loader = DataLoader(dataset=valset, batch_size=9, shuffle=False)
 test_loader = DataLoader(dataset=testset, batch_size=9, shuffle=True)
 
 
-# In[19]:
+# In[20]:
 
 
 print(len(trainset[0]))
@@ -229,14 +264,14 @@ print(img.permute(1, 2, 0).shape)
 plt.imshow(img.permute(1, 2, 0))
 
 
-# In[20]:
+# In[21]:
 
 
 plt.imshow(testset[0][0].permute(1, 2, 0))
 testset[0][0].shape
 
 
-# In[21]:
+# In[22]:
 
 
 # display 9 imgs
@@ -247,12 +282,12 @@ def display_image(imgs, labels, true_labels):
     for i in range(9):
         imgs_=imgs[i]
         axes[i].set_title(label_dict[str(labels[i].item())]+", "+label_dict[str(true_labels[i].item())])
-        imgs_ = imgs_[:, :, :]
+        imgs_ = imgs_[:]
 #         print(imgs_.shape)
         axes[i].imshow(np.reshape(imgs_, (256, 256, 3)))
 
 
-# In[22]:
+# In[23]:
 
 
 ## Check torch.max(outputs, 1)[1]
@@ -262,7 +297,7 @@ print(val_imgs.shape)
 display_image(val_imgs, val_labels, val_labels)
 
 
-# In[23]:
+# In[24]:
 
 
 def to_gpu(resource):
@@ -273,7 +308,11 @@ def to_gpu(resource):
             return resource
     except:
         return resource
-    
+
+
+# In[25]:
+
+
 ## without softmax : nn.CrossEntropyLoss uses softmax in their side, we don't need it in our network
 def prepResnet18(num_classes=2, pretrained=True):
     resnet18 = models.resnet18(pretrained=pretrained, progress = True)
@@ -285,15 +324,35 @@ def prepResnet50(num_classes=2, pretrained=True, imageNet=False):
     resnet50.fc = torch.nn.Linear(resnet50.fc.in_features, num_classes)
     return resnet50
 
+def prepVgg19(bn=True):
+    num_classes = 2
+    if not bn:
+        vgg19 = models.vgg19(pretrained=True, progress = True)
+    else:
+        vgg19 = models.vgg19_bn(pretrained=True, progress = True)
+        vgg19.classifier.add_module("7", torch.nn.Dropout(p=0.4))
+        vgg19.classifier.add_module("8", torch.nn.Linear(1000, num_classes))
+    return vgg19
 
-# In[24]:
+
+# In[26]:
 
 
-model=prepResnet18()
+# modelDict = {1:"Resnet18",
+#              2:"Resnet50",
+#              3:"Vgg19"
+
+modelFn = config["modelFn"]
+if modelFn == "Resnet18":
+    model=prepResnet18()
+elif modelFn == "Resnet50":
+    model=prepResnet50()
+elif modelFn == "Vgg19":
+    model=prepVgg19()
 model=to_gpu(model)
 
 
-# In[25]:
+# In[27]:
 
 
 def accuracy(loader, batch_size, model):
@@ -317,33 +376,19 @@ def accuracy(loader, batch_size, model):
     return (correct/len_output)*100
 
 
-# In[26]:
+# In[28]:
 
 
 accuracy(val_loader, 9, model)
 
 
-# In[27]:
+# In[29]:
 
 
 accuracy(test_loader, 9, model)
 
 
-# In[28]:
-
-
-def prepVgg19(bn=True):
-    num_classes = 2
-    if not bn:
-        vgg19 = models.vgg19(pretrained=True, progress = True)
-    else:
-        vgg19 = models.vgg19_bn(pretrained=True, progress = True)
-        vgg19.classifier.add_module("7", torch.nn.Dropout(p=0.4))
-        vgg19.classifier.add_module("8", torch.nn.Linear(1000, num_classes))
-    return vgg19
-
-
-# In[29]:
+# In[30]:
 
 
 def checkChildren(layer):
@@ -351,6 +396,7 @@ def checkChildren(layer):
         return list(layer.children())
     else:
         return False
+
 def checkWeight(layer):
     if hasattr(layer, "weight"):
         return True
@@ -362,24 +408,31 @@ def getWeight(layer):
         print(layer, " : ", layer.weight.requires_grad)
     children=checkChildren(layer)
     for child in children:
-        checkWeight(child)
         getWeight(child)
+# getWeight(model)
 
+# def checkAttr(layer, attr):
+#     if hasattr(layer, attr):
+#         return True
+#     else:
+#         return False
 
-# In[30]:
-
-
-l1=list(model.layer3[0].children())
-for child in l1:
-    if hasattr(child, "weight"):
-        print(child, " : ", child.weight.requires_grad)
+# def getWeight_(layer):
+#     children=[]
+#     if checkAttr(layer, "weight"):  ## checkAttr(layer, "weight")
+#         print(layer, " : ", layer.weight.requires_grad)
+#     if checkAttr(layer, "children"):  ## checkAttr(layer, "children")
+#         children=list(layer.children())
+#     for child in children:
+#         getWeight_(child)
+# getWeight_(model)
 
 
 # In[31]:
 
 
-def initializeResnet18(model, layers=None, freeze_all=False):
-    """`freeze all` has priority over `layers`"""
+def initializeModel(model, modelFn, layers=None, lr=0.001, optimizer=torch.optim.SGD, weight=None, freeze_all=False):
+    """`freeze all` has priority over `layers`. layers only works for Resnet18"""
     if layers == []:
         layers = None
     smallestChild = np.inf
@@ -395,14 +448,15 @@ def initializeResnet18(model, layers=None, freeze_all=False):
         for param in params:
             param.requires_grad_(freeze_all)
     elif layers:
-        for layer in layers:
-            getattr(model, "layer"+str(layer)).requires_grad_(False)
-            if layer < smallestChild:
-                smallestChild = layer
-            if layer > largestChild:
-                largestChild = layer
-        firstChildren = list(model.children())[:smallestChild+3] # Resnet has 0-3 layers before the blocks
-        secondChildren = list(model.children())[4+largestChild:] # 3+1 = 4
+        if modelFn == "Resnet18":
+            for layer in layers:
+                getattr(model, "layer"+str(layer)).requires_grad_(False)
+                if layer < smallestChild:
+                    smallestChild = layer
+                if layer > largestChild:
+                    largestChild = layer
+            firstChildren = list(model.children())[:smallestChild+3] # Resnet has 0-3 layers before the blocks
+            secondChildren = list(model.children())[4+largestChild:] # 3+1 = 4
 
         params=[]
         for ele in firstChildren:
@@ -411,15 +465,21 @@ def initializeResnet18(model, layers=None, freeze_all=False):
             params += ele.parameters()
     getWeight(model) 
     print(len(params))
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.CrossEntropyLoss(weight=weight)
     optimizer = torch.optim.SGD(params,lr = 0.001)
     return criterion, optimizer
+
+# def initializeResnet50():
+#     pass
+
+# def initializeVgg19():
+#     pass
 
 
 # In[32]:
 
 
-def initializeModel(model, freeze_all=False):
+def initializeModel_(model, freeze_all=False):
     for param in model.parameters():
         param.requires_grad_(not freeze_all)
 #     model.fc.requires_grad_(True)
@@ -428,7 +488,7 @@ def initializeModel(model, freeze_all=False):
         params = model.fc.parameters()
     else:
         params = model.parameters()
-    optimizer = torch.optim.Adam(params,lr = 0.001)
+    optimizer = torch.optim.Adam(params, lr = 0.001)
     return criterion, optimizer
 
 
@@ -437,26 +497,34 @@ def initializeModel(model, freeze_all=False):
 
 ## RESNET18
 # criterion, optimizer = initializeResnet18(model, 2, False)
-criterion, optimizer = initializeResnet18(model, [1, 2, ], False)
-# print(resnet50)
+# criterion, optimizer = initializeResnet18(model, [1, ], False)
+if config["weights"]:
+    weight = to_gpu(torch.FloatTensor([3875/(1341+3875), 1341/(1341+3875)]))
+else:
+    weight = None
+
+modelFn = config["modelFn"]
+criterion, optimizer = initializeModel(model, modelFn, config["layerFreeze"], config["lr"], config["optimizer"], weight)
+# if modelFn == "Resnet18":
+#     criterion, optimizer = initializeModel(model, modelFn, config["layerFreeze"], config["lr"], config["optimizer"], weight)
+# elif modelFn == "Resnet50":
+#     criterion, optimizer = initializeModel(model, modelFn, config["layerFreeze"], config["lr"], config["optimizer"], weight)
+# elif modelFn == "Vgg19":
+#     criterion, optimizer = initializeModel()
+
+# criterion, optimizer = initializeResnet18(model, config["layerFreeze"], config["lr"], config["optimizer"], weight)
 
 
 # In[34]:
 
 
-# print(T)
+### Train
 
 
 # In[35]:
 
 
-### Train
-
-
-# In[36]:
-
-
-def train(n_epochs, resnet18, loader, criterion, optimizer, weight):
+def train(n_epochs, resnet18, loader, criterion, optimizer, weight=None):
 
     loss_avg = []
     loss_=[]
@@ -475,7 +543,9 @@ def train(n_epochs, resnet18, loader, criterion, optimizer, weight):
             images, labels = to_gpu(images), to_gpu(labels)
             optimizer.zero_grad()
             outputs = resnet18(images)
-#             loss = F.cross_entropy(outputs, labels.long(), weight=weight)
+#             if weight:
+#                 loss = F.cross_entropy(outputs, labels.long(), weight=weight)
+#             else:
             loss = criterion(outputs, labels.long())
             loss.backward()
             optimizer.step()
@@ -501,7 +571,7 @@ def train(n_epochs, resnet18, loader, criterion, optimizer, weight):
     return r_loss, resnet18, acc_list, loss_list, loss_, loss_avg
 
 
-# In[37]:
+# In[36]:
 
 
 #### 
@@ -509,7 +579,7 @@ def train(n_epochs, resnet18, loader, criterion, optimizer, weight):
 # 2) Run with new loss function : loss = criterion(torch.max(outputs, 1)[1], labels.long())
 
 
-# In[38]:
+# In[37]:
 
 
 # # 1) Run and check loss with the existing train function : loss = criterion(outputs, labels.long())
@@ -520,7 +590,7 @@ def train(n_epochs, resnet18, loader, criterion, optimizer, weight):
 # print((time.time()-t1)/60)
 
 
-# In[39]:
+# In[38]:
 
 
 batchSize = trainBatchSize
@@ -529,20 +599,14 @@ dFactor = batchSize/64
 dFactor
 
 
-# In[ ]:
+# In[39]:
 
 
-
-
-
-# In[40]:
-
-
-e_epoch=3
+e_epoch=4
 # e_epoch=3
 
 
-# In[41]:
+# In[40]:
 
 
 # 1) Run and check loss with the existing train function : loss = criterion(outputs, labels.long())
@@ -550,12 +614,12 @@ e_epoch=3
 import time
 t1=time.time()
 weight = to_gpu(torch.FloatTensor([3875/(1341+3875), 1341/(1341+3875)]))
-r_loss, model, acc_list, loss_list, loss, loss_avg= train(n_epochs=e_epoch, resnet18=model, loader=train_loader,
+r_loss, model, acc_list, loss_list, loss, loss_avg= train(n_epochs=config["epochs"], resnet18=model, loader=train_loader,
                                                              criterion=criterion, optimizer=optimizer, weight=weight)
 print((time.time()-t1)/60)
 
 
-# In[42]:
+# In[41]:
 
 
 # # iter(train_loader)
@@ -563,35 +627,35 @@ print((time.time()-t1)/60)
 # print(len(val_data))
 
 
-# In[43]:
+# In[42]:
 
 
 ## Run again!! Epoch = 4
 num_list = []
-for i in range(e_epoch):
+for i in range(config["epochs"]):
     num_list += list(np.linspace(i, i+1 ,int(82/dFactor) , endpoint=False))
 
 
-# In[44]:
+# In[43]:
 
 
 len(num_list)
 
 
-# In[45]:
+# In[44]:
 
 
 len(num_list[0:4075])
 
 
-# In[46]:
+# In[45]:
 
 
 print(len(num_list))
 print(len(loss_list))
 
 
-# In[47]:
+# In[46]:
 
 
 ## Find Accuracy on Test Dataset!!
@@ -600,7 +664,7 @@ acc = accuracy(test_loader, 9, model)
 print("accurarcy : ", acc)
 
 
-# In[48]:
+# In[47]:
 
 
 train_loader = DataLoader(dataset=trainset, batch_size=32, shuffle=True)
@@ -608,7 +672,7 @@ acc = accuracy(train_loader, 32, model)
 print("accuracy : ", acc)
 
 
-# In[49]:
+# In[48]:
 
 
 ## Run again!! Epoch = 4
@@ -616,16 +680,16 @@ print("accuracy : ", acc)
 plt.plot(num_list[0:len(loss_list)], loss_list)
 
 
+# In[49]:
+
+
+plt.plot([i for i in range(config["epochs"])], loss)
+
+
 # In[50]:
 
 
-plt.plot([i for i in range(e_epoch)], loss)
-
-
-# In[51]:
-
-
-plt.plot([i for i in range(e_epoch)], loss_avg)
+plt.plot([i for i in range(config["epochs"])], loss_avg)
 
 
 # In[ ]:
